08cc63ac6188f9e1df565dbd59763fd4e75acd59
bedirhansay <bedirhan.sayy@gmail.com>
2022-04-09 04:37:23 +0300

    R Programming

---
+++ Temel_islemler/Documents/2.1_temel_islemler.R
@@ -0,0 +1,12 @@
+# Temel Islemler
+
+
+## Operatorler : <<- , <- ve = ifaddesi atama yaparken kullanilir. = ifadesi daha cok fonk. icinde atama yaparken kullanilir. 
+
+### Atama Operatorleri
+
+
+### Aritmetik Operatorler
+
+
+### Mantiksel Operatorler 
